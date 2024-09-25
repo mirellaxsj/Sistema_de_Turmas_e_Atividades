53586b6a5b2eac737f3793b3985e27e5d3fe47b7
JMarques12 <joaovcmarques12@gmail.com>
2024-09-25 11:13:03 -0300

    aula 07

--- bd/scipt.sql
+++ bd/scipt.sql
@@ -1,1 +1,15 @@
-drop database if exists
+drop database if exists Escola;
+create database Escola CHARSET=UTF8 COLLATE utf8_general_ci;
+use Escola
+
+create table Professor {
+    id varchar(5) not null primary key,
+    nome varchar(50) not null,
+    descricao text,
+    valor decimal(10,2) not null
+}
+
+create table Turmas {
+    id int not null primany key,
+    
+}
